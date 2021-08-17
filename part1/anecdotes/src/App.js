8ee03876ed9d2123e310aface5c090cfb2a477b6
camalow <62999432+camalow@users.noreply.github.com>
2021-08-17 14:08:59 +0500

    Part1 Ex1.14 done

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import anecdotes from './anecdotes'
+
+const DisplayAnecdote = ({ header, anecdote, votes }) =>
+  <div>
+    <h3>{header}</h3>
+    <p>{anecdote}</p>
+    <p>has {votes} votes</p>
+  </div>
+
+const Button = ({ handleFunction, text }) =>
+  <button onClick={handleFunction}>{text}</button>
+
+const App = () => {
+  const [state, setState] = useState({
+    selected: 0,
+    votes: new Array(anecdotes.length).fill(0)
+  })
+
+  const randNum = Math.floor(Math.random() * anecdotes.length)
+
+  const nextAnecdote = () => setState({ ...state, selected: randNum })
+
+  const handleVote = () => {
+    const copy = [...state.votes]
+    copy[state.selected]++
+    setState({ ...state, votes: copy })
+  }
+
+  const mostVoted = state.votes.reduce((a, b) => Math.max(a, b))
+
+  return (
+    <div>
+      <DisplayAnecdote
+        header="Anecdote of the day"
+        anecdote={anecdotes[state.selected]}
+        votes={state.votes[state.selected]} />
+
+      <Button handleFun={handleVote} text="vote" />
+
+      <Button handleFun={nextAnecdote} text="next anecdote" />
+      <DisplayAnecdote
+        header="anecdote with most votes"
+        anecdote={anecdotes[state.votes.indexOf(mostVoted)]}
+        votes={mostVoted} />
+
+    </div>
+  )
+}
+
+export default App
---
+++ part1/anecdotes/src/anecdotes.js
@@ -0,0 +1,19 @@
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'The best way to get a project done faster is to start sooner',
+    'Even the best planning is not so omniscient as to get it right the first time.',
+    'How does a project get to be a year late?... One day at a time.',
+    'The bearing of a child takes nine months, no matter how many women are assigned. Many software tasks have this characteristic because of the sequential nature of debugging.',
+    'Design and programming are human activities; forget that and all is lost.',
+    'Plan to throw one (implementation) away; you will, anyhow.',
+    'Before software can be reusable it first has to be usable.',
+    'Real programmers can write assembly code in any language.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+]
+
+export default anecdotes

7c812f0d9958410b079f0beae3034b1bbeccc75f
camalow <62999432+camalow@users.noreply.github.com>
2021-08-19 17:20:47 +0500

    Part2 Ex2.20 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import services from './services/phone_book_service'
-import { Filter, PersonForm, Persons } from './components'
+import { Filter, PersonForm, Persons, Notification } from './components'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewnumber] = useState('')
   const [newFilter, setNewfilter] = useState('')
+  const [notifMessage, setNotifMessage] = useState({})
   const handleNameInput = e => setNewName(e.target.value)
   const handleNumberInput = e => setNewnumber(e.target.value)
   const filterFun = e => setNewfilter(e.target.value)
@@ -26,27 +27,42 @@ const App = () => {
       if (window.confirm(`${newName} is alrady added to phonebook, replace the old number with a new one?`)) {
         services
           .put(alreadyExist.id, { ...alreadyExist, number: newNumber })
-          .then(res =>
+          .then(res => {
             setPersons(
               persons.map(el => el.id === alreadyExist.id ? el = res : el)
             )
-          )
+            setNotifMessage({ text: `${newName}'s number has changed`, err: false })
+            setTimeout(() => setNotifMessage({ text: '', err: false }), 3000)
+          })
+          .catch(err => {
+            setNotifMessage({ text: `${newName} has already been removed from server`, err: true })
+            setTimeout(() => setNotifMessage({ text: '', err: false }), 3000)
+          })
       }
     }
     else {
       const postObject = { name: newName, number: newNumber }
       services
         .post(postObject)
-        .then(res => { setPersons(persons.concat(res)); console.log(res); })
+        .then(res => {
+          setPersons(persons.concat(res))
+          setNotifMessage({ text: `Added ${newName}`, err: false })
+          setTimeout(() => setNotifMessage({ text: '', err: false }), 3000)
+        })
     }
   }
 
   const handleDelete = id => () => {
-    if (window.confirm(`Delete ${persons.find(p => p.id === id).name}`))
+    const person = persons.find(p => p.id === id).name
+    if (window.confirm(`Delete ${person}`))
       services
         .del(id)
         .then(services.getAll)
         .then(res => setPersons(res))
+        .catch(err => {
+          setNotifMessage({ text: `${newName} has already been removed from server`, err: true })
+          setTimeout(() => setNotifMessage({ text: '', err: false }), 3000)
+        })
   }
 
   const showPersons = persons.filter(person =>
@@ -55,6 +71,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notifMessage} />
       <Filter filterFun={filterFun} />
       <h2>Add a new</h2>
       <PersonForm submitFun={addName}
--- part2/phonebook/src/components.js
+++ part2/phonebook/src/components.js
@@ -2,13 +2,19 @@ import React from 'react'
 
 export const Filter = ({ filterFun }) => <div>filter shown with <input onChange={filterFun} /></div>
 
-export const PersonForm = ({ submitFun, nameOnchange, numberOnchange }) =>
-    <form onSubmit={submitFun}>
-        <div>name: <input onChange={nameOnchange} /></div>
-        <div>number: <input onChange={numberOnchange} /></div>
-        <div><button type="submit">add</button></div>
-    </form>
+export const PersonForm = ({ submitFun, nameOnchange, numberOnchange }) => {
 
+    return (
+        <form style={{
+            margin: 20,
+            fontSize: 20
+        }} onSubmit={submitFun}>
+            <div>name: <input onChange={nameOnchange} /></div>
+            <div>number: <input onChange={numberOnchange} /></div>
+            <div><button type="submit">add</button></div>
+        </form>
+    )
+}
 export const Persons = ({ persons, handleDelete }) =>
     <div>
         {persons.map(person =>
@@ -17,4 +23,25 @@ export const Persons = ({ persons, handleDelete }) =>
             </p>)}
     </div>
 
+export const Notification = ({ message }) => {
+    const styleObj = {
+        display: 'inline-block',
+        color: 'rgb(98, 15, 153)',
+        fontStyle: 'italic',
+        padding: 10,
+        margin: 5,
+        fontSize: 20,
+        border: `2px solid ${message.err ? 'red' : 'green'}`,
+        borderRadius: 3,
+        background: `rgba(76, ${message.err ? '10' : '150'}, 80, 0.3)`,
+    }
+    if (message.text)
+        return (
+            <div style={styleObj}>
+                {message.text}
+            </div>
+        )
+    else
+        return <div></div>
+}
 

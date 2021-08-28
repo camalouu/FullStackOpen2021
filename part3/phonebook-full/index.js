0eb0a2681ba29b488063ef7d0efdadfeb379f271
camalow <62999432+camalow@users.noreply.github.com>
2021-08-28 15:35:06 +0500

    Part3 Ex3.18 done

--- part3/phonebook-full/index.js
+++ part3/phonebook-full/index.js
@@ -18,32 +18,39 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-    Person.count().then(number => {
+    Person.countDocuments().then(number => {
         res.send(`Phonebook has info for ${number} people <br> ${new Date()}`)
     })
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     Person.findById(req.params.id)
-        .then(person => res.json(person))
+        .then(person => {
+            if (person)
+                res.json(person)
+            else
+                res.status(404).end()
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    const person = data.find(p => p.id == id)
-    if (person) {
-        data = data.filter(p => p.id !== person.id)
-        res.status(200).end()
-    } else {
-        res.status(404).end()
-    }
+
+app.delete('/api/persons/:id', (req, res, next) => {
+
+    Person.findByIdAndDelete(req.params.id)
+        .then(result => {
+            console.log(result)
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
+
 
 app.post('/api/persons', (req, res) => {
     const name = req.body.name
     const number = req.body.number
 
-    if (!name || !number)
+    if (!(name && number))
         return res.status(400).send({ error: 'name and number are required' })
 
     const newPerson = new Person({
@@ -53,6 +60,34 @@ app.post('/api/persons', (req, res) => {
 
     newPerson.save().then(_ => res.send(newPerson))
 })
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const person = {
+        name: req.body.name,
+        number: req.body.number
+    }
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(result => {
+            if (result)
+                res.json(result)
+            else
+                res.status(404).end();
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+app.use(errorHandler)
+
+const unknownEndpoint = (req, res) =>
+    res.status(404).send({ error: 'unknown endpoint' })
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`App is working on ${PORT}`))

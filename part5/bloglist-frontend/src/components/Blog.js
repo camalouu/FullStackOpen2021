9142851d64a97ec9345944212bb18bee4141f096
camalow <62999432+camalow@users.noreply.github.com>
2021-10-19 10:10:41 +0500

    Part5 Ex5.14 done

---
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import blogService from '../services/blogs'
+
+
+const Blog = ({ blog, user }) => {
+
+  const [details, setDetails] = useState(false)
+  const [likes, setLikes] = useState(blog.likes)
+  const [showBlog, setShowBlog] = useState(true)
+
+  const blogStyle = {
+    display: showBlog ? '' : 'none',
+    paddingTop: 10,
+    paddingLeft: 5,
+    border: 'solid',
+    borderWidth: 2,
+    marginBottom: 5
+  }
+
+  const removeButtonStyle = { display: user.username === blog.user.username ? '' : 'none' }
+
+  const handleLike = async () => {
+    const newBlog = {
+      ...blog,
+      user: blog.user.id,
+      likes: likes + 1
+    }
+    await blogService.updateBlog(newBlog)
+    setLikes(likes + 1)
+  }
+
+  const handleRemove = async () => {
+    if (window.confirm(`Remove blog ${blog.title}`)) {
+      await blogService.deleteBlog(blog)
+      setShowBlog(false)
+    }
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setDetails(!details)}>
+          {details ? 'hide' : 'view'}
+        </button>
+      </div>
+      {
+        details ?
+          <div>
+            <div>{blog.url}</div>
+            <div>{likes} <button onClick={handleLike}>like</button></div>
+            <div>{blog.user.username}</div>
+            <button onClick={handleRemove} style={removeButtonStyle}>remove</button>
+          </div> :
+          <div />
+      }
+    </div>
+  )
+}
+
+export default Blog
---
+++ part5/bloglist-frontend/src/components/tests/blogs.test.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent } from '@testing-library/react'
+import Blog from '../Blog'
+
+const blog = {
+  author: 'someone',
+  title: 'jealousy',
+  url: 'sdfs.com',
+  likes: 23,
+  user: { username: 'zafar' }
+}
+const user = {
+  username: 'zafar'
+}
+
+test('blog title and author', () => {
+
+  const component = render(<Blog blog={blog} user={user} />)
+  expect(component.container).toHaveTextContent('someone')
+  expect(component.container).toHaveTextContent('jealousy')
+  expect(component.container).not.toHaveTextContent('sdfs.com')
+  expect(component.container).not.toHaveTextContent('like')
+
+})
+
+
+test('url and likes are shown when btn clicked', () => {
+  const component = render(<Blog blog={blog} user={user} />)
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  const url = component.getByText(blog.url)
+  expect(url).toBeDefined()
+
+  const likes = component.getByText(blog.likes)
+  expect(likes).toBeDefined()
+})

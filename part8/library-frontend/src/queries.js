8f8a6f1a6996251fdb978c128a46d04813cb52ed
camalow <62999432+camalow@users.noreply.github.com>
2021-11-18 10:45:54 +0500

    Part8 Ex8.8 done

---
+++ part8/library-frontend/src/queries.js
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client'
+
+export const ALL_AUTHORS = gql`
+    query AllAuthors {
+        allAuthors {
+            name
+            id
+            born
+            bookCount
+        }
+    }
+`

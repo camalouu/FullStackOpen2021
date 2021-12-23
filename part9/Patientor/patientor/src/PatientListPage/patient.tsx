47b5f8b68b9c4869500a2cc00be69709c8470a23
camalow <62999432+camalow@users.noreply.github.com>
2021-12-23 11:49:04 +0500

    Part9 Ex9.17 done

---
+++ part9/Patientor/patientor/src/PatientListPage/patient.tsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useStateValue } from "../state";
+import { Patient } from "../types";
+import { apiBaseUrl } from "../constants";
+import { Icon } from "semantic-ui-react";
+
+const PatientInfo = () => {
+    const { id } = useParams<{ id: string }>();
+    const [{ patients }, dispatch] = useStateValue();
+    useEffect(() => {
+        if (!patients[id].ssn) {
+            void axios
+                .get<Patient>(`${apiBaseUrl}/patients/${id}`)
+                .then(
+                    ({ data }) => dispatch({ type: "UPDATE_PATIENT", payload: data })
+                );
+        }
+    }, []);
+    return (
+        <>
+            <h2>
+                {patients[id].name} {patients[id].gender == "female" ? <Icon name="venus" /> : <Icon name="mars" />}
+            </h2>
+            <p>ssn: {patients[id].ssn}</p>
+            <p>occupation: {patients[id].occupation}</p>
+        </>
+    );
+};
+
+export default PatientInfo;
